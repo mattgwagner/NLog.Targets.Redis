8c581a9951f981519e0459602e0477bad62db920
Matt Wagner <mattgwagner@gmail.com>
2014-08-30 07:21:44 -0400

    Switching to use StackExchange.Redis instead of ServiceStack

--- RedisTarget.cs
+++ RedisTarget.cs
@@ -1,63 +1,103 @@
-﻿namespace NLog.Targets.Redis
-{
-    using ServiceStack.Redis;
-    using System;
-    using System.ComponentModel.DataAnnotations;
+﻿using StackExchange.Redis;
+using System;
+using System.ComponentModel.DataAnnotations;
 
+namespace NLog.Targets.Redis
+{
     [Target("Redis")]
     public class RedisTarget : TargetWithLayout
     {
+        private readonly Lazy<RedisConnection> _redis;
+
         public String Host { get; set; }
 
         public int Port { get; set; }
 
-        private volatile RedisClient redisClient;
-        private string dataType;
+        public int Db { get; set; }
+
+        public String Password { get; set; }
 
         [Required]
-        public string Key { get; set; }
+        public String Key { get; set; }
 
-        [Required]
-        public string DataType 
-        {   
-            get { return this.dataType; }
-            set { this.dataType = value.ToLower(); } 
+        private string _dataType;
+
+        public String DataType
+        {
+            get { return this._dataType; }
+            set { this._dataType = value.ToLower(); }
         }
 
         public RedisTarget()
         {
             this.Host = "localhost";
             this.Port = 6379;
+            this.Db = 0;
+            this.Password = String.Empty;
+
             this.DataType = "list";
-        }
 
-        protected RedisClient RedisClient
-        {
-            get
+            this._redis = new Lazy<RedisConnection>(() =>
             {
-                if (redisClient == null)
-                {
-                    lock (SyncRoot)
-                    {
-                        if (redisClient == null)
-                        {
-                            redisClient = new RedisClient(this.Host, this.Port);
-                        }
-                    }
-                }
-
-                return redisClient;
-            }
+                return new RedisConnection(this.Host, this.Port, this.Db, this.Password);
+            });
         }
 
         protected override void Write(LogEventInfo logEvent)
         {
-            if (dataType.Equals("list"))
-                this.RedisClient.PushItemToList(this.Key, Layout.Render(logEvent));
-            else if (dataType.Equals("channel"))
-                this.RedisClient.PublishMessage(this.Key, Layout.Render(logEvent));
+            if ("list" == this._dataType)
+            {
+                this._redis.Value.GetClient().ListLeftPushAsync(this.Key, Layout.Render(logEvent));
+            }
+            else if ("channel" == this._dataType)
+            {
+                this._redis.Value.GetClient().PublishAsync(this.Key, Layout.Render(logEvent));
+            }
             else
-                throw new InvalidOperationException("Invalid type for redis operation: " + this.dataType);
+            {
+                throw new InvalidOperationException("Invalid DataType for Redis NLog Target");
+            }
+        }
+    }
+
+    public class RedisConnection
+    {
+        // Note: I'm writing this for redis without clustering
+
+        private readonly ConnectionMultiplexer _connectionManager;
+
+        private readonly String _host;
+        private readonly int _port;
+        private readonly int _db;
+
+        public RedisConnection(string host = "localhost", int port = 6379, int db = 0, string password = null)
+        {
+            this._host = host;
+            this._port = port;
+            this._db = db;
+
+            var options = new ConfigurationOptions
+            {
+                Password = password,
+                AbortOnConnectFail = false,
+                ConnectRetry = 5,
+                ConnectTimeout = 1000,
+                KeepAlive = 10
+            };
+
+            options.EndPoints.Add(this._host, this._port);
+
+            this._connectionManager = ConnectionMultiplexer.Connect(options);
+        }
+
+        public RedisConnection(ConnectionMultiplexer connectionMultiplexer)
+        {
+            this._connectionManager = connectionMultiplexer;
+        }
+
+        public IDatabase GetClient()
+        {
+            return this._connectionManager.GetDatabase(this._db);
         }
     }
 }
